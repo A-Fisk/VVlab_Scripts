6770319d7514e32ad17ea59bc3f60194fa604c6b
A-Fisk <angus.fisk@bnc.ox.ac.uk>
2018-06-07 16:29:23 +0100

    Revert "7_6_18"
    This reverts commit 43441e8584470e9928e52eb1494f531652d0a3f0.

--- Angus_working_Scripts/a_1_Read_rewritten.m
+++ Angus_working_Scripts/a_1_Read_rewritten.m
@@ -24,9 +24,6 @@ num_channels=4;
 %type of recording/output signal
 recording_type = 'EEG';
 
-%Visualise variable for resmpling 
-visualise = 0;
-
 
 
 %%Constants for reading from tanks
@@ -66,7 +63,7 @@ tanks_list = tank_specific_dir_obj(3:(length(tank_specific_dir_obj)-1));
 
 
 %Step two, create directory to hold outputs 
-output_path = [global_directory, 'Output_1\'];
+output_path = [global_directory, 'Output_1'];
 
 %only make output if doesn't exist
 if exist(output_path, 'dir') ~= 7
@@ -80,19 +77,22 @@ end
 %for single day
 %for single mouse
 
+current_block_string = [tank_specific_dir, tanks_list(5).name];
+
 % for loop to go through each block in turn - same as dd in VVscript
 %looping through all the blocks in the directory 
+<<<<<<< HEAD
 for item_no=1:length(tanks_list)
+=======
+for item_no=1%:length(tanks_list)
+>>>>>>> parent of 43441e8... 7_6_18
     
    %assigns block variable to be equal to the block name string
    block = tanks_list(item_no).name;
    
-   %create variable of day name so we can save easily
-   day_name_string = tanks_list(item_no).name(6:11);
-   
    
    %%%%%%% Development, just doing with one animal
-   for mouse=1:num_animals
+   for mouse=1%:num_animals
        
        %create string of which part of tank to read
        %want to read 'EEG1' etc, not EEr. 
@@ -129,7 +129,7 @@ for item_no=1:length(tanks_list)
            0,...Sortcode - disregards all sort codes
            0,...T1 - starts reading at start of block
            1,...T2 - reads events until 1 - ???
-           'ALL'); %Options - read all events records in range
+           'ALL') %Options - read all events records in range
        
        %Read the sampling rate
        record_sampling_rate = TTX.ParseEvInfoV(0,...Starting index of records
@@ -140,13 +140,77 @@ for item_no=1:length(tanks_list)
        output_sampling_rate = 256;
        
        
+<<<<<<< HEAD
        %Create new list which is timestamps with 0 at the starts
        Timestamps_list = [0:step:1e+6];
+=======
+       %Now going to read the data from the tank+block in steps to avoid
+       %overloading memory. 
+       %While reading the events, we are going to grab the timestamps of
+       %the events and put them in a separate list
+       %The purpose of this loop is to get the correct number of events in
+       %the timestamps_list so that we can read the continuous events later
+       %making sure we get all the events.
+       
+       %firstly create some event counters 
+       step_counter = 1; 
+       events = 1;
+       %create empty list to hold the timestamps
+       timestamp_list = [];
+       
+       %while loop as we will update the events variable each time through
+       %the loop, this ensures we keep reading until there are no more
+       %events to read 
+       while events > 0
+           
+           %define the start and end steps 
+           start_step = ((step_counter-1)*step)
+           end_sep = (step_counter*step)
+           
+           %Read events in steps 
+           events = TTX.ReadEventsV(max_events_return,... max events to return, sky high
+               events_to_read,... string of which events to read
+               0,... Channel - returns all channels
+               0,... Sortcode - disregards all sort codes
+               start_step,...where to start reading
+               end_step,... where to finish reading
+               'ALL');%Read all events in range
+           
+           %If there are still events being read
+           if (events > 0)
+              
+               %read the timestamps from the just read events - cached
+               %currently
+               timestamps = TTX.ParseEvInfoV(0,... Start at the start of the cache
+                   events,... read the timestamps for the events we just read
+                   6); %read just the timestamps
+               
+               %update the timestamp list with newly read
+               timestamp_list = cat(2,...dimension to concatenate along
+                   timestamp_list,...original timestamp list
+                   timestamps(1,end)); %all the timestamps just read
+               
+           end %end if events are present statement
+           
+           %increment the step counter
+           step_counter = step_counter + 1
+                         
+       end %end while events loop
+       
+       %Create new list which is timestamps with 0 at the starts
+       Timstamps_list_1 = [0, timestamp_list];
+       %clear old list to go through loop again for next animal - don't
+       %actually need as we redefine as an empty list 
+       clear timestamp_list;
+       
+>>>>>>> parent of 43441e8... 7_6_18
        
        %Next step is to read the continuous variables waves
        %this needs to be done in chunks as otherwise will overload the
        %memory 
+       for 
        
+<<<<<<< HEAD
        %loop through all the different channels to read each one and save
        %it separately 
        for channel=1:num_channels
@@ -237,6 +301,15 @@ for item_no=1:length(tanks_list)
            %memory 
            
        end %end of channels loop
+=======
+       
+       
+       
+       
+        
+       
+   
+>>>>>>> parent of 43441e8... 7_6_18
    
    end % end mouse loop
    
